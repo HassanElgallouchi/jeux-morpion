0998a626304c11a63240b5a2ecb40c0aea883d24
HassaElgallouchi <helgallouchi@gmail.com>
2021-03-06 19:16:01 +0100

    add all possible condotion to win

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const buttons = document.querySelectorAll("#game>div>button");
 const gameStatus = document.querySelector("#game_status");
-
 let joueurActuel = "O";
 let statusGagnant = false;
+
+let funcDernierGagnant = () => {
+    if (window.localStorage.getItem("dernier_gagnant")) {
+        let dernierGagnant = JSON.parse(window.localStorage.getItem("dernier_gagnant"));
+        let d = dernierGagnant;
+        console.log(d);
+        let gameId = document.querySelector("#game");
+        gameId.appendChild(document.createElement("div")).setAttribute("class", "gagnant");
+        let divDernierGagnant = document.querySelector(".gagnant");
+        divDernierGagnant.style.backgroundColor = "#fff";
+        divDernierGagnant.style.marginTop = "10px";
+        divDernierGagnant.style.padding = "10px";
+        divDernierGagnant.innerHTML = `<strong>Dernier gagnant : </strong> ${dernierGagnant}`;
+    }
+}
 
 let handleEvent = (e) => {
     let joueurO = "O";
@@ -24,11 +38,7 @@ let handleEvent = (e) => {
 }
 
 let main = () => {
-
-
     funcDernierGagnant();
-
-
     buttons.forEach(videButton => {
         videButton.innerHTML = "";
         videButton.style.backgroundColor = "";
@@ -39,7 +49,6 @@ let main = () => {
 }
 
 let rejouer = () => {
-
     let btnRejouer = document.querySelector('#game_status a');
     btnRejouer.addEventListener('click', () => {
         statusGagnant = false;
@@ -49,6 +58,7 @@ let rejouer = () => {
 }
 
 function check(symboleJoueur) {
+    // Premier ligne
     if (
         buttons[0].textContent == symboleJoueur &&
         buttons[1].textContent == symboleJoueur &&
@@ -59,28 +69,115 @@ function check(symboleJoueur) {
         buttons[2].style.backgroundColor = "green";
         gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
         statusGagnant = true;
-        rejouer(symboleJoueur);
-
-
+        rejouer();
         window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
         funcDernierGagnant()
     }
-}
+    // Deuxiéme ligne
+    if (
+        buttons[3].textContent == symboleJoueur &&
+        buttons[4].textContent == symboleJoueur &&
+        buttons[5].textContent == symboleJoueur
+    ) {
+        buttons[3].style.backgroundColor = "green";
+        buttons[4].style.backgroundColor = "green";
+        buttons[5].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
+    // Troisiéme ligne
+    if (
+        buttons[6].textContent == symboleJoueur &&
+        buttons[7].textContent == symboleJoueur &&
+        buttons[8].textContent == symboleJoueur
+    ) {
+        buttons[6].style.backgroundColor = "green";
+        buttons[7].style.backgroundColor = "green";
+        buttons[8].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
 
-let funcDernierGagnant = () => {
-    if (window.localStorage.getItem("dernier_gagnant")) {
-        let dernierGagnant = JSON.parse(window.localStorage.getItem("dernier_gagnant"));
-        let d = dernierGagnant;
-        console.log(d);
-        let gameId = document.querySelector("#game");
-        gameId.appendChild(document.createElement("div")).setAttribute("class", "gagnant");
-        let divDernierGagnant = document.querySelector(".gagnant");
-        divDernierGagnant.style.backgroundColor = "#fff";
-        divDernierGagnant.style.marginTop = "10px";
-        divDernierGagnant.style.padding = "10px";
-
-        divDernierGagnant.innerHTML = `<strong>Dernier gagnant : </strong> ${dernierGagnant}`;
-
+    // Premier colonne
+    if (
+        buttons[0].textContent == symboleJoueur &&
+        buttons[3].textContent == symboleJoueur &&
+        buttons[6].textContent == symboleJoueur
+    ) {
+        buttons[0].style.backgroundColor = "green";
+        buttons[3].style.backgroundColor = "green";
+        buttons[6].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
+    // Deuxiéme colonne
+    if (
+        buttons[1].textContent == symboleJoueur &&
+        buttons[4].textContent == symboleJoueur &&
+        buttons[7].textContent == symboleJoueur
+    ) {
+        buttons[1].style.backgroundColor = "green";
+        buttons[4].style.backgroundColor = "green";
+        buttons[7].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
+    // Troisiéme colonne
+    if (
+        buttons[2].textContent == symboleJoueur &&
+        buttons[5].textContent == symboleJoueur &&
+        buttons[8].textContent == symboleJoueur
+    ) {
+        buttons[2].style.backgroundColor = "green";
+        buttons[5].style.backgroundColor = "green";
+        buttons[8].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
+    // Premiére diagonale left top to right bottom 
+    if (
+        buttons[0].textContent == symboleJoueur &&
+        buttons[4].textContent == symboleJoueur &&
+        buttons[8].textContent == symboleJoueur
+    ) {
+        buttons[0].style.backgroundColor = "green";
+        buttons[4].style.backgroundColor = "green";
+        buttons[8].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
+    }
+    // Deusiéme diagonale right top to left bottom
+    if (
+        buttons[2].textContent == symboleJoueur &&
+        buttons[4].textContent == symboleJoueur &&
+        buttons[6].textContent == symboleJoueur
+    ) {
+        buttons[2].style.backgroundColor = "green";
+        buttons[4].style.backgroundColor = "green";
+        buttons[6].style.backgroundColor = "green";
+        gameStatus.innerHTML = `<a href="#">Rejouer ?</a> <br/> Le joueur ${symboleJoueur} a gagné !`;
+        statusGagnant = true;
+        rejouer();
+        window.localStorage.setItem("dernier_gagnant", JSON.stringify(symboleJoueur));
+        funcDernierGagnant()
     }
 }
 
